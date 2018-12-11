3dcc114655ad6019e004b808a12a9fe50a195e5f
Donna <donnamcla@gmail.com>
2018-12-11 07:43:12 -0500

    remove redux, remove some of the CRA artifacts

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <header className="App-header"></header>
         <ConsolidatedScreeningContainer />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
