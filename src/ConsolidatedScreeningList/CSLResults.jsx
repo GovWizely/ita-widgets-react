5f6360251f439857f2f0697ec9329cdaf1b56989
Donna <donnamcla@gmail.com>
2018-12-11 17:26:18 -0500

    toggleable table for more details

--- src/ConsolidatedScreeningList/CSLResults.jsx
+++ src/ConsolidatedScreeningList/CSLResults.jsx
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
+import CSLDetails from './CSLDetails';
 
 class CSLResults extends Component {
-  constructor() {
-    super()
-    this.state = {
-      toggleDetails: false,
-    };
-  }
-
-  handleClick = () => {
-    this.setState({ toggleDetails: !this.state.toggleDetails })
-  }
 
   render() {
     const moreInfoUrl = "http://export.gov/ecr/eg_main_023148.asp";
 
     const resultsList = this.props.results.map(item => {
       return (
-        <div key={item.id}>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <p><a href="#" onClick={this.handleClick}> {item.name}</a></p>
-          { this.state.toggleDetails ? <p>details component</p> : null}
-          {/* [TODO] need to make each item its own component so we can control which details menu is toggled */}
-        </div>
+        <CSLDetails 
+          key={item.id}
+          onClick={this.handleClick}
+          name={item.name}
+          remarks={item.remarks}
+          source={item.source}
+          altNames={item.alt_names}
+        />
       )
     })
-    
+
     return (
       <div className="resultsList">
         <h3>{this.props.total} results - <a href={moreInfoUrl}>More Information About the Results</a></h3>
