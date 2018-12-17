1acac2b99a8e5844eb538e14095e38efbba110ff
Donna <donnamcla@gmail.com>
2018-12-17 11:01:10 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-// import CSLContainer from './ConsolidatedScreeningList/CSLContainer';
-// import SearchContainer from './SearchWidget/SearchContainer';
-import SearchContainerWithDropDown from './SearchWidget/SearchContainer';
+import SearchContainer from './SearchWidget/SearchContainer';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <CSLContainer /> */}
-        {/* <SearchContainer /> */}
-        <SearchContainerWithDropDown endpoint="trade_leads"/>
+        <SearchContainer endpoint="trade_leads"/>
         {/* possible endpoints: "consolidated_screening_list" or "trade_leads" */}
       </div>
     );
