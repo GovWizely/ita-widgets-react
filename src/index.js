bbb69ef3f2cd120d2e280dd436965298cb9b8b8e
Donna <donnamcla@gmail.com>
2018-12-21 18:17:10 -0500

    move declaration of endpoint and API_KEY to the <head> of the html

--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom';
 import SearchContainer from './SearchWidget/SearchContainer';
 import * as serviceWorker from './serviceWorker';
 
-/* possible endpoints: "consolidated_screening_list", "trade_leads", "trade_events", "export_assistance_centers", "international_office_locations" */
 
-const endpoint="consolidated_screening_list";
-// const endpoint="trade_leads";
-// const endpoint="trade_events";
-// const endpoint="export_assistance_centers";
-// const endpoint="international_office_locations";
-
-const API_KEY="ShCzzrAkXLpMTsTlhFhUjD29";
-
-ReactDOM.render(<SearchContainer endpoint={endpoint} API_KEY={API_KEY} />, document.getElementById('root'));
+ReactDOM.render(<SearchContainer endpoint={window.endpoint} API_KEY={window.API_KEY} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
