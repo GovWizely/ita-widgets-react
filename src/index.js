b6ad578e47ad51e670a02de396f12f5192e6304a
Donna <donnamcla@gmail.com>
2019-01-03 10:51:49 -0500

    display all the widgets together

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import SearchContainer from './SearchWidget/SearchContainer';
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(<SearchContainer endpoint={window.endpoint} API_KEY={window.API_KEY} />, document.getElementById('widget-container'));
+// ReactDOM.render(<SearchContainer endpoint={window.endpoint} API_KEY={window.API_KEY} />, document.getElementById('widget-container'));
+ReactDOM.render(
+  <div className="root">
+    <h1>ITA Search Widgets</h1>
+    <SearchContainer endpoint="consolidated_screening_list" API_KEY={window.API_KEY} />
+    <br/><hr/>
+    <SearchContainer endpoint="trade_leads" API_KEY={window.API_KEY} />
+    <br/><hr/>
+    <SearchContainer endpoint="trade_events" API_KEY={window.API_KEY} />
+    <br/><hr/>
+    <SearchContainer endpoint="export_assistance_centers" API_KEY={window.API_KEY} />
+    <br/><hr/>
+    <SearchContainer endpoint="international_office_locations" API_KEY={window.API_KEY} />
+    <br/><hr/>
+  </div>
+  , document.getElementById('widget-container'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
