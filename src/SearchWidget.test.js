4d7db535e2f09a7b5f3812d2c0a88b9fa93e4e34
Donna Hogan <ddhogan@users.noreply.github.com>
2019-01-07 18:23:37 -0500

    [EDSP-579] Browser tests (#2)
    * swap test packages
    
    * minor formatting change
    
    * browser automation tests
    
    * remove the "add this widget" instructions
    
    * scope all styles to avoid conflicts when embedding in other pages

--- src/SearchWidget.test.js
+++ src/SearchWidget.test.js
@@ -1,45 +1,121 @@
-import React from 'react';
-import { configure, shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import SearchContainer from './SearchWidget/SearchContainer';
-import SearchResults from './SearchWidget/SearchResults';
-import SearchDetails from './SearchWidget/SearchDetails';
+const puppeteer = require('puppeteer');
 
-import * as mockProps from './mockProps';
-import * as mockState from './mockState';
-import widgetInfo from './widgetInfo';
+let browser;
+let page;
 
-configure ({adapter: new Adapter()});
+beforeAll(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.goto('http://localhost:3000');
+});
 
-describe('endpoint="consolidated_screening_list"', () => {
+test('main title is present', async () => {
+  const mainTitleElement = '.root > h1';
+  const mainTitleText = await page.$eval(mainTitleElement, e => e.innerHTML);
+  await page.waitForSelector(mainTitleElement);
+  expect(mainTitleText).toBe('ITA Search Widgets');
+})
 
-  it('<SearchDetails/> should render a table and heading containing the info when receiving a search result item', () => {
-    let wrapper = shallow(<SearchDetails />).setState({toggleDetails: true}).setProps(mockProps.CSLDetails);
-    // expect(wrapper.find('td')).toHaveLength(8);
-    // expect(wrapper.html()).toEqual("<div><p><a href=\"#\" title=\"Expand details table\" aria-label=\"Expand details table\">Bobby Droptables</a></p><table><tbody><tr><td>Name</td><td>Bobby Droptables</td></tr><tr><td>Remarks</td><td></td></tr><tr><td>Source</td><td>Denied Persons List (DPL) - Bureau of Industry and Security</td></tr><tr><td>Alt Names</td><td>Smith</td></tr></tbody></table></div>");
-    expect(wrapper.contains(<td>Bobby Droptables</td>)).toBe(true);
+describe('consolidated screening list', () => {
+  test('shows the title', async () => {
+    const titleElement = '#widget-container > div > div:nth-child(3) > form > h3';
+    const titleText = await page.$eval(titleElement, e => e.innerHTML);
+    await page.waitForSelector(titleElement);
+    expect(titleText).toBe('Search the Consolidated Screening List:');
   });
 
-  it('<SearchResults/> should render the <SearchDetails/> component along with the number of results', () => {
-    let context = {widgetInfo: widgetInfo};
-    let wrapper = shallow(<SearchResults {...mockProps.CSLResults} />, {context});
-    expect(wrapper.find(SearchDetails)).toHaveLength(2);
-    expect(wrapper.contains(<p>2 results <a href="http://export.gov/ecr/eg_main_023148.asp"><i> More Information About the Results</i></a></p>)).toBe(true);
-  });
-
-  it('<SearchContainer/> should render <SearchResults/> component', () => {
-    let context = {widgetInfo: widgetInfo};
-    let wrapper = shallow(<SearchContainer {...mockProps.CSLContainer} />, {context}).setState(mockState.CSLContainer);
-    expect(wrapper.find(SearchResults));
-    expect(wrapper.contains(<h3>Search The Consolidated Screening List:</h3>));
+  test('searching returns results', async () => {
+    const inputField = "#widget-container > div > div:nth-child(3) > form > input[type='text']";
+    const searchButton = "#widget-container > div > div:nth-child(3) > form > button";
+    const resultsHeader = "#widget-container > div > div:nth-child(3) > div.resultsList > p"; // this element will be present if successful, even if it's zero results
+    await page.waitForSelector(inputField);
+    await page.type(inputField, "Smith");
+    await page.click(searchButton);
+    await page.waitForSelector(resultsHeader, 20000);
+    await page.click('#clearButton');
   });
 });
 
-describe('endpoint="export_assistance_centers"', () => {
-  it('<SearchDetails/> should render the link & table with address correctly', () => {
-    let wrapper = shallow(<SearchDetails />).setState({toggleDetails: true}).setProps(mockProps.EACDetails);
-    // expect(wrapper.contains(<td>03857 - Ye Olde Office</td>)).toBe(true);
-    // expect(wrapper.html()).toEqual("<div><p><a href=\"#\" title=\"Expand details table\" aria-label=\"Expand details table\">03857 - Ye Olde Office</a></p><table><tbody><tr><td>Zip Code</td><td>03857</td></tr><tr><td>Office Name</td><td>Ye Olde Office</td></tr><tr><td>Address</td><td><div>line1</div><div>Portsmouth, New Hampshire 03801</div></td></tr><tr><td>Email</td><td>email@address</td></tr><tr><td>Phone</td><td>555-1234</td></tr></tbody></table></div>");
-    expect(wrapper.contains(<td><div>line1</div><div>Portsmouth, New Hampshire 03801</div></td>)).toBe(true);
+describe('trade leads', () => {
+  test('shows title and a dropdown menu', async ()=> {
+    const titleElement = "#widget-container > div > div:nth-child(6) > form > h3";
+    const dropdownMenu = "#widget-container > div > div:nth-child(6) > form > div.Dropdown";
+    await page.waitForSelector(dropdownMenu);
+    const titleText = await page.$eval(titleElement, e => e.innerHTML);
+    expect(titleText).toBe('Search Trade Leads:');
   });
-})
+
+  test('searching with a text query returns results', async () => {
+    const inputField = "#widget-container > div > div:nth-child(6) > form > input[type='text']";
+    const searchButton = "#widget-container > div > div:nth-child(6) > form > button";
+    const resultsHeader = "#widget-container > div > div:nth-child(6) > div.resultsList > p";
+    await page.waitForSelector(inputField);
+    await page.type(inputField, "computer");
+    await page.click(searchButton);
+    await page.waitForSelector(resultsHeader, 20000);
+    await page.click('#clearButton');
+  });
+});
+
+describe('trade events', () => {
+  test('shows title and a dropdown menu', async ()=> {
+    const titleElement = "#widget-container > div > div:nth-child(9) > form > h3";
+    const dropdownMenu = "#widget-container > div > div:nth-child(9) > form > div.Dropdown";
+    const titleText = await page.$eval(titleElement, e => e.innerHTML);    
+    await page.waitForSelector(dropdownMenu);
+    expect(titleText).toBe('Search Trade Events:');
+  });
+
+  test('searching with a text query returns results', async () => {
+    const inputField = "#widget-container > div > div:nth-child(9) > form > input[type='text']";
+    const searchButton = "#widget-container > div > div:nth-child(9) > form > button";
+    const resultsHeader = "#widget-container > div > div:nth-child(9) > div.resultsList > p";
+    await page.waitForSelector(inputField);
+    await page.type(inputField, "computer");
+    await page.click(searchButton);
+    await page.waitForSelector(resultsHeader, 20000);
+    await page.click('#clearButton');
+  });
+});
+
+describe('export assistance centers', () => {
+  test('shows title', async ()=> {
+    const titleElement = "#widget-container > div > div:nth-child(12) > form > h3";
+    const titleText = await page.$eval(titleElement, e => e.innerHTML);    
+    expect(titleText).toBe('Search Export Assistance Centers:');
+  });
+
+  test('searching with a zipcode returns results', async () => {
+    const inputField = "#widget-container > div > div:nth-child(12) > form > input[type='text']";
+    const searchButton = "#widget-container > div > div:nth-child(12) > form > button";
+    const resultsHeader = "#widget-container > div > div:nth-child(12) > div.resultsList > p";
+    await page.waitForSelector(inputField);
+    await page.type(inputField, "99950");
+    await page.click(searchButton);
+    await page.waitForSelector(resultsHeader, 20000);
+    await page.click('#clearButton');
+  });
+});
+
+describe('international office locations', () => {
+  test('shows title and a dropdown menu', async ()=> {
+    const titleElement = "#widget-container > div > div:nth-child(15) > form > h3";
+    const dropdownMenu = "#widget-container > div > div:nth-child(15) > form > div.Dropdown";
+    const titleText = await page.$eval(titleElement, e => e.innerHTML);    
+    await page.waitForSelector(dropdownMenu);
+    expect(titleText).toBe('Search International Office Locations:');
+  });
+
+  test('searching with a text query returns results', async () => {
+    const inputField = "#widget-container > div > div:nth-child(15)> form > input[type='text']";
+    const searchButton = "#widget-container > div > div:nth-child(15) > form > button";
+    const resultsHeader = "#widget-container > div > div:nth-child(15) > div.resultsList > p";
+    await page.waitForSelector(inputField);
+    await page.type(inputField, "Portsmouth");
+    await page.click(searchButton);
+    await page.waitForSelector(resultsHeader, 20000);
+    await page.click('#clearButton');
+  });
+});
+
+afterAll(() => browser.close());
